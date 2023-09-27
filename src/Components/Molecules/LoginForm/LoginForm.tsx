5de8ea281731a7eb45bd8c2559eb8c35597ba82b
2Junsu <ijh1205@naver.com>
2023-09-27 17:24:27 +0900

    ISSUE#10 feat: 로그인 api 연동 코드 작성

--- src/Components/Molecules/LoginForm/LoginForm.tsx
+++ src/Components/Molecules/LoginForm/LoginForm.tsx
@@ -1,19 +1,54 @@
-import { FormEvent } from "react";
+import { ChangeEvent, FormEvent, useState } from "react";
+
+import Loading from "@/components/atoms/Loading";
+import { useAxios } from "@/hooks/useAxios";
 
 import { LoginButton, StyledForm } from "./LoginForm.style";
 import Input from "../Input/Input";
 
 const LoginForm = () => {
+	const { response, isLoading, refetch } = useAxios({
+		url: "/posts",
+		method: "get",
+	});
+
+	const [formValue, setFormvalue] = useState({
+		email: "",
+		password: "",
+	});
+
+	const { email, password } = formValue;
+
+	const isFilled = !!email.trim() && !!password.trim();
+
+	const handleChangeInput = (e: ChangeEvent<HTMLFormElement>) => {
+		const changed = {
+			...formValue,
+			[e.target.name]: e.target.value,
+		};
+		setFormvalue(changed);
+	};
+
 	const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
-		//login
 		e.preventDefault();
+		refetch();
+		if (!response) return;
+		//login
 	};
 
 	return (
-		<StyledForm onSubmit={handleSubmit}>
-			<Input inputProps={{ placeholder: "이메일" }} />
-			<Input inputProps={{ placeholder: "비밀번호", type: "password" }} />
-			<LoginButton>로그인</LoginButton>
+		<StyledForm onChange={handleChangeInput} onSubmit={handleSubmit}>
+			<Input inputProps={{ name: "email", placeholder: "이메일" }} />
+			<Input
+				inputProps={{
+					name: "password",
+					placeholder: "비밀번호",
+					type: "password",
+				}}
+			/>
+			<LoginButton disabled={isLoading || !isFilled}>
+				{isLoading ? <Loading /> : "로그인"}
+			</LoginButton>
 		</StyledForm>
 	);
 };
