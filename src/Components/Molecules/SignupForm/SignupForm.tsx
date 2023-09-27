275786c086ee4c261bea3a8cfcedbd36eadb6178
2Junsu <ijh1205@naver.com>
2023-09-27 18:49:49 +0900

    ISSUE#10 feat: 회원가입 api 연동 코드 작성 및 유효성 검증 로직  추가

--- src/Components/Molecules/SignupForm/SignupForm.tsx
+++ src/Components/Molecules/SignupForm/SignupForm.tsx
@@ -1,15 +1,64 @@
-import { FormEvent, useState } from "react";
+import { ChangeEvent, FormEvent, useEffect, useState } from "react";
 
-import { ConsentAreaDiv, SignupButton, StyledForm } from "./SignupForm.style";
+import Loading from "@/components/atoms/Loading";
+import { useAxios } from "@/hooks/useAxios";
+import { UserType } from "@/types/user";
+
+import {
+	ConsentAreaDiv,
+	ErrorSpan,
+	InputWrapDiv,
+	SignupButton,
+	StyledForm,
+} from "./SignupForm.style";
 import ConsentCheckWithDetails from "../ConsentCheckWithDetails/ConsentCheckWithDetails";
 import Input from "../Input/Input";
 
+interface SignupFormType extends UserType {
+	passwordCheck: string;
+}
+
 const SignupForm = () => {
+	const { response: responseForDuplication, refetch: refetchDuplication } =
+		useAxios({
+			url: "중복체크",
+			method: "POST",
+		});
+	const {
+		response: responseForSignup,
+		refetch: refetchSignup,
+		isLoading,
+	} = useAxios({
+		url: "회원가입",
+		method: "POST",
+	});
+
+	const [formValue, setFormvalue] = useState<SignupFormType>({
+		email: "",
+		password: "",
+		passwordCheck: "",
+		kindergarden: "",
+		kidsName: "",
+	});
+	const [emailErrorMsg, setEmailErrorMsg] = useState("");
+	const [pwErrorMsg, setPwErrorMsg] = useState("");
+	const [pwCheckErrorMsg, setPwCheckErrorMsg] = useState("");
 	const [isConsented, setIsConsented] = useState<boolean[]>([false, false]);
+
+	const { email, password, passwordCheck, kindergarden, kidsName } = formValue;
 
 	const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
 		//signup
 		e.preventDefault();
+		// refetchSignup();
+	};
+
+	const handleChangeInput = (e: ChangeEvent<HTMLFormElement>) => {
+		const changed = {
+			...formValue,
+			[e.target.name]: e.target.value,
+		};
+		setFormvalue(changed);
 	};
 
 	const handleConsent = (index: number) => {
@@ -18,13 +67,106 @@ const SignupForm = () => {
 		);
 	};
 
+	const validateEmail = () => {
+		if (email === "") {
+			setEmailErrorMsg("");
+			return;
+		}
+
+		const emailRegex = /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/;
+		if (!emailRegex.test(email) && email !== "") {
+			setEmailErrorMsg("올바른 이메일 형식이 아닙니다.");
+			return;
+		}
+
+		//중복체크
+		// refetchDuplication();
+	};
+
+	const checkPasswordEnable = () => {
+		const passwordRegex = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,20}$/;
+		return passwordRegex.test(password);
+	};
+
+	const validatePassword = () => {
+		if (password === "") {
+			setPwErrorMsg("");
+			return;
+		}
+
+		if (checkPasswordEnable()) {
+			setPwErrorMsg("사용 가능한 비밀번호입니다.");
+		} else {
+			setPwErrorMsg("8자 이상 20자 이하의 영문 및 숫자로 입력해주세요.");
+		}
+	};
+
+	const validatePasswordCheck = () => {
+		if (passwordCheck === "") {
+			setPwCheckErrorMsg("");
+			return;
+		}
+
+		if (password !== passwordCheck) {
+			setPwCheckErrorMsg("비밀번호가 일치하지 않습니다.");
+		} else {
+			setPwCheckErrorMsg("비밀번호가 일치합니다.");
+		}
+	};
+
+	//이메일 사용가능 조건 추가
+	const checkSubmitEnable = () =>
+		checkPasswordEnable() &&
+		password === passwordCheck &&
+		kindergarden.trim() &&
+		kidsName.trim() &&
+		isConsented[0] &&
+		isConsented[1];
+
+	useEffect(() => {
+		validateEmail();
+	}, [email]);
+
+	useEffect(() => {
+		validatePassword();
+	}, [password]);
+
+	useEffect(() => {
+		validatePasswordCheck();
+	}, [password, passwordCheck]);
+
 	return (
-		<StyledForm onSubmit={handleSubmit}>
-			<Input inputProps={{ placeholder: "이메일" }} />
-			<Input inputProps={{ placeholder: "비밀번호", type: "password" }} />
-			<Input inputProps={{ placeholder: "비밀번호 확인", type: "password" }} />
-			<Input inputProps={{ placeholder: "소속" }} />
-			<Input inputProps={{ placeholder: "아이 이름" }} />
+		<StyledForm onChange={handleChangeInput} onSubmit={handleSubmit}>
+			<InputWrapDiv>
+				<Input inputProps={{ name: "email", placeholder: "이메일" }} />
+				<ErrorSpan>{emailErrorMsg}</ErrorSpan>
+			</InputWrapDiv>
+			<InputWrapDiv>
+				<Input
+					inputProps={{
+						name: "password",
+						placeholder: "비밀번호",
+						type: "password",
+					}}
+				/>
+				{<ErrorSpan $isEnable={checkPasswordEnable()}>{pwErrorMsg}</ErrorSpan>}
+			</InputWrapDiv>
+			<InputWrapDiv>
+				<Input
+					inputProps={{
+						name: "passwordCheck",
+						placeholder: "비밀번호 확인",
+						type: "password",
+					}}
+				/>
+				{
+					<ErrorSpan $isEnable={password === passwordCheck}>
+						{pwCheckErrorMsg}
+					</ErrorSpan>
+				}
+			</InputWrapDiv>
+			<Input inputProps={{ name: "kindergarden", placeholder: "소속" }} />
+			<Input inputProps={{ name: "kidsName", placeholder: "아이 이름" }} />
 			<ConsentAreaDiv>
 				<ConsentCheckWithDetails
 					title="키즈가든 서비스 이용약관 동의"
@@ -37,7 +179,9 @@ const SignupForm = () => {
 					isChecked={isConsented[1]}
 				/>
 			</ConsentAreaDiv>
-			<SignupButton>회원가입</SignupButton>
+			<SignupButton disabled={!checkSubmitEnable() || isLoading}>
+				{isLoading ? <Loading /> : "회원가입"}
+			</SignupButton>
 		</StyledForm>
 	);
 };
