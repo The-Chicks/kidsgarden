dad98f5b6ba901a9244382b39d7f266ee5dccbb1
2Junsu <ijh1205@naver.com>
2023-10-03 19:30:51 +0900

    ISSUE#14 feat: 로그인 임시 구현

--- src/Components/Molecules/LoginForm/LoginForm.tsx
+++ src/Components/Molecules/LoginForm/LoginForm.tsx
@@ -1,17 +1,20 @@
 import { ChangeEvent, FormEvent, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
 
 import Loading from "@/components/atoms/Loading";
 import { useAxios } from "@/hooks/useAxios";
 
 import { LoginButton, StyledForm } from "./LoginForm.style";
 import Input from "../Input/Input";
-
 interface LoginFormType {
 	email: string;
 	password: string;
 }
 
 const LoginForm = () => {
+	const navigate = useNavigate();
+
 	const { response, isLoading, refetch } = useAxios({
 		url: "/posts",
 		method: "get",
@@ -36,8 +39,13 @@ const LoginForm = () => {
 
 	const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
 		e.preventDefault();
-		refetch();
-		if (!response) return;
+		if (email === "ijh1205@naver.com" && password === "wnstn0630") {
+			navigate("/");
+		} else {
+			toast.error("이메일 또는 비밀번호가 잘못되었습니다.");
+		}
+		// refetch();
+		// if (!response) return;
 		//login
 	};
 
